51673ac89a1b037310107f44c65b997b7abe6f9d
m13corona <mcorona1308@gmail.com>
2024-01-27 15:28:24 -0600

    add current date at the top through html and js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -21,3 +27,7 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var today = dayjs();
+$('#currentDay').text (today.format('[Today is] dddd MMMM D, YYYY'))
+
