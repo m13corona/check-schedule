eb0e66b6509b5ed149ecd32b985f6f9df0784b00
m13corona <mcorona1308@gmail.com>
2024-01-28 23:25:05 -0600

    add and remove code in the HTML and JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+//these are important DOM elements
+var hour9El = $('#hour-9');
+var hour10El = $('#hour-10');
+var hour11El = $('#hour-11');
+var hour12El = $('#hour-12');
+var hour13El = $('#hour-13');
+var hour14El = $('#hour-14');
+var hour15El = $('#hour-15');
+var hour16El = $('#hour-16');
+var hour17El = $('#hour-17');
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+var presentTime = dayjs().format('H');
+presentTime ()
 
 
 
@@ -12,6 +27,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+//these are the event listeners for the save buttons of every hour 
 
 var hour9El = $('#hour-9');
 var hour10El = $('#hour-10');
@@ -21,13 +37,47 @@ var hour13El = $('#hour-13');
 var hour14El = $('#hour-14');
 var hour15El = $('#hour-15');
 var hour16El = $('#hour-16');
-var hour7El = $('#hour-17');
+var hour17El = $('#hour-17');
 
 
 hour9El.on('click', function () {
-  alert('Hello')
-
+  
 });
+
+hour10El.on('click', function () {
+  alert('Hello')
+});
+
+hour11El.on('click', function () {
+  alert('Hello')
+});
+
+hour12El.on('click', function () {
+  alert('Hello')
+});
+
+hour13El.on('click', function () {
+  alert('Hello')
+});
+
+hour14El.on('click', function () {
+  alert('Hello')
+});
+
+hour15El.on('click', function () {
+  alert('Hello')
+});
+
+hour16El.on('click', function () {
+  alert('Hello')
+});
+
+hour17El.on('click', function () {
+  alert('Hello')
+});
+
+
+
 
 
 
@@ -37,7 +87,21 @@ hour9El.on('click', function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+function hourRightNow (){;
+  if ((dayjs) !== presentTime) ;
+  $('hour-9').css({'color: red'});
+
+}
+
+
+
+
+// if it is this time, make this x color. use dom traversal for this
+var hourPresent = dayjs.format('h:mm:ss a');
+hourPresent();
+
+if dayjs = (9 am)
 
 
 
